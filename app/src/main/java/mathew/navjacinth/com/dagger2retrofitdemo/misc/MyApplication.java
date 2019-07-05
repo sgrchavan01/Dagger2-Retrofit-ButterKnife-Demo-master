088f85a69ac69769019a36d467268c2e9f06fb2f
unknown <sgrchavan01@gmail.com>
2019-07-05 19:07:37 +0530

    first commit

---
+++ app/src/main/java/mathew/navjacinth/com/dagger2retrofitdemo/misc/MyApplication.java
@@ -0,0 +1,28 @@
+package mathew.navjacinth.com.dagger2retrofitdemo.misc;
+
+import android.app.Application;
+
+import mathew.navjacinth.com.dagger2retrofitdemo.dagger.component.DaggerRetrofitComponent;
+import mathew.navjacinth.com.dagger2retrofitdemo.dagger.component.RetrofitComponent;
+import mathew.navjacinth.com.dagger2retrofitdemo.dagger.module.RetrofitModule;
+
+/**
+ * Created by Navjacinth Mathew on 1/29/2017.
+ */
+
+public class MyApplication extends Application {
+
+    private RetrofitComponent retrofitComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        retrofitComponent = DaggerRetrofitComponent.builder()
+                .retrofitModule(new RetrofitModule())
+                .build();
+    }
+
+    public RetrofitComponent getRetrofitComponent() {
+        return retrofitComponent;
+    }
+}
